6e41a6ecb64d606129230b29ed24fb4818edd6bc
UmeeAndrey <andrey@umee.cc>
2023-01-31 17:27:07 -0800

    1️⃣ first commit!

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,32 @@
 import Head from 'next/head';
+import Image from 'next/image'
+import { MAIL_CHIMP_STRING_FORM } from '../mail-chimp';
 import styles from '../styles/Home.module.css';
+import btcKing from '../public/btc-king.gif'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>CryptoJobs.guide: Find crypto jobs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to CryptoJobs.guide!
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+        <p>
+          We are working hard to develop the best possible blockchain and defi career hub! Subscribe to our newsletter for updates. Coming soon!
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div>
+          <Image
+            src={btcKing}
+            alt="CryptoJobs.guide"
+            width="350px"
+            height="300px"
+          /></div>
+        <div dangerouslySetInnerHTML={{ __html: MAIL_CHIMP_STRING_FORM }} />
       </main>
 
       <footer>
